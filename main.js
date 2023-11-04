af1c8bac07e1e2c5233b23031ff72356608c86dd
thelastenvoy <satvikbatra@gmail.com>
2023-11-04 15:46:41 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const http = require('http');
+
+function fetchFromAPI(url) {
+  // TODO: Return a promise that resolves to the response from the API
+}
+
+function saveToFile(filePath, data) {
+  // TODO: Use fs.writeFile to save the data to the specified file
+}
+module.exports = function fetchFromAPI(url, filePath){ };
+
+// TODO: Call fetchFromAPI and saveToFile with the command-line arguments
